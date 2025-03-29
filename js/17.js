97231b6c7714f2054d36f3241816a539a0a510c3
SC-Sistemas <sistemas@segurosdelascalifornias.com>
2025-03-28 17:55:19 -0700

    ola

---
+++ js/17.js
@@ -0,0 +1,10 @@
+//Array metodos
+const tecnologias = ['HTML','CSS','JavaScript','React.js','Node.js']
+const numero =[10,20,30]
+
+//Filter
+//Sirve para sacar un elemento del arreglo, o mantener un elemento en el arreglo y sacar los demas
+
+const nuevoArray = tecnologias.filter(function(tech){
+    console.log(tech)
+})
